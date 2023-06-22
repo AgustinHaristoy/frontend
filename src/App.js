06fb0381540c1c576fb2346b0a0e0db9ae6efcfc
Agustin Haristoy <agustin.haristoy@usach.cl>
2023-06-22 19:28:55 -0400

    Historia de usuario 1 con frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeComponent from './components/HomeComponent.jsx';
+import ArchivosComponent from './components/ArchivosComponent.jsx';
+import FileUploadAcopioComponent from './components/FileUploadAcopioComponent';
+import FileInformationAcopioComponent from './components/FileInformationAcopioComponent';
+import FileUploadGrasaSolidoComponent from './components/FileUploadGrasaSolidoComponent';
+import FileInformationGrasaSolidoComponent from './components/FileInformationGrasaSolidoComponent';
+import ProveedorComponent from './components/ProveedorComponent';
+import PagosComponent from './components/PagosComponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/archivos" element={<ArchivosComponent />} />
+        <Route path= "/subir-archivo-acopio" element={<FileUploadAcopioComponent />} />
+        <Route path= "/informacion-archivo-acopio" element={<FileInformationAcopioComponent />} />
+        <Route path= "/subir-archivo-grasasolido" element={<FileUploadGrasaSolidoComponent />} />
+        <Route path= "/informacion-archivo-grasasolido" element={<FileInformationGrasaSolidoComponent />} />
+        <Route path= "/proveedores" element={<ProveedorComponent />} />
+        <Route path= "/planilla-pagos" element={<PagosComponent />} />
+
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
